41bd7b360312fa644942eb9db52e0396eba648ed
LarexGame <renealexandergarciatriana@gmail.com>
2025-09-29 10:17:04 -0700

    Print the Hello forms

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by Rene Garcia 9/29
+
+#ask user for a name
+name = input("What is your name?")
+
+# print hello to the user
+
+print ("Hello " + name)
+print ("Hello {0}".format(name))
+print ("Hello", name)
+print (f'Hello {name}')
+
+message = "Hello " + name
+print (message)
+
