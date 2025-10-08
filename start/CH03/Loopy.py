475109db28d383efd2dc62372da1c4838b90431a
LarexGame <renealexandergarciatriana@gmail.com>
2025-10-08 08:51:07 -0700

    loops

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By Rene Garcia 10/08
+
+def hi():
+    for i in range(3):
+        print("Hello World")
+        print(i)
+
+print("Starting")
+hi()
+print("Done!")
