19a1f523a9d6b7e857435c7ebb7429f7faaa87be
LarexGame <renealexandergarciatriana@gmail.com>
2025-10-15 09:40:32 -0700

    Read Files

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
 # By 
+
+import os
+
+# Get current director
+
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+
+# Build File Path
+
+file_path = os.path.join(script_dir, "testfile.txt")
+
+# Create File Object
+
+f = open(file_path, "r")
+
+# Read to file object
+
+print(f.read())
+
+# Close file Object
+
+f.close()
