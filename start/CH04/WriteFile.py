b6f88d8c9faad52eb8d44a57c67f4229812374cf
LarexGame <renealexandergarciatriana@gmail.com>
2025-10-15 09:30:33 -0700

    Write File

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -2,3 +2,30 @@
 # Sample script that writes to a file
 # By Garcia 10/15
 
+
+
+
+import os
+
+# Get current director
+
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+
+# Build File Path
+
+file_path = os.path.join(script_dir, "testfile.txt")
+
+# Create File Object
+
+f = open(file_path, "w")
+
+# Write to file object
+
+f.write("My Name is Alex\n")
+f.write("I like rubber ducks\n")
+f.write("Hello World\n")
+
+# Close file Object
+
+f.close()
