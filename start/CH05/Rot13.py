e968dc1d07dd8346c93973657dbb8c2c6ee38e01
LarexGame <renealexandergarciatriana@gmail.com>
2025-10-20 09:44:50 -0700

    Convertion to Cryption

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -3,4 +3,32 @@
 # By Garcia 10/20
 
 
+# Get Message from user
+message = input("What is the message? ")
+message = message.upper()
+final_message = ""
 
+# From each letter in message
+for letter in message:
+    # convert letter to number
+    converted_letter = ord(letter)
+
+    # Check for letter
+    if converted_letter >= 65 and converted_letter <=90:
+
+        # Add 13 to number
+        converted_letter += 13
+        # Past Z?
+        if converted_letter > 90:
+            # Subtract 26 from number
+            converted_letter -= 26
+        # Convert number to letter
+        upt_letter = chr(converted_letter)
+        final_message += upt_letter
+
+    else:
+        final_message += letter
+
+
+# Print updated Message
+print(final_message)
