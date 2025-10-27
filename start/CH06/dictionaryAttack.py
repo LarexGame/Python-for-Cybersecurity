d3abe84501b61139a4aa58da016ca3a231d92186
LarexGame <renealexandergarciatriana@gmail.com>
2025-10-27 10:15:31 -0700

    2 Ways Dictionary Attack

--- start/CH06/dictionaryAttack.py
+++ start/CH06/dictionaryAttack.py
@@ -48,13 +48,54 @@ dictionary_list = read_dictionary("top10.txt")
 # For each line in dictionary
 for guess in dictionary_list:
     guess = guess.strip()
-    # hash teh guess
+    # hash the guess
     guess_hash = hash_password(hash_type, salt, guess)
     # Is it a match
     if guess_hash == password_hash:
-        # Print guess abd quit
+        # Print guess and quit
         print(f"Password Found: {guess}")
         # How to quit the Script?
         sys.exit(0)
 
-print("Oassword not found")
+print("Password not found")
+
+
+#######################################
+########## POSIBBLE FIXING ############
+#######################################
+
+import os
+import sys
+from passlib.hash import sha512_crypt
+
+# Functions
+def hash_password(salt, password):
+    return sha512_crypt.using(salt=salt, rounds=5000).hash(password)
+
+def read_dictionary(file_name):
+    script_path = os.path.abspath(__file__)
+    script_dir = os.path.dirname(script_path)
+    file_path = os.path.join(script_dir, file_name)
+    with open(file_path, "r") as f:
+        return f.readlines()
+
+# Get Hash string
+password_hash = input("What is the password hash: ")
+
+# Split hash
+hash_parts = password_hash.split("$")
+hash_type = hash_parts[1]
+salt = hash_parts[2]
+
+# Load Dictionary file
+dictionary_list = read_dictionary("top10.txt")
+
+# Try each guess
+for guess in dictionary_list:
+    guess = guess.strip()
+    guess_hash = hash_password(salt, guess)
+    if guess_hash == password_hash:
+        print(f"Password Found: {guess}")
+        sys.exit(0)
+
+print("Password not found")
