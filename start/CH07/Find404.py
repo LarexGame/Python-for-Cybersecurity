6f427513188649a5eed316d4899350cc6bff2384
LarexGame <renealexandergarciatriana@gmail.com>
2025-11-03 09:40:28 -0800

    Find404 and Brakes in

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for 404 errors
-# By 
+# By Garcia 11/03
+
+
+import os
+
+# Ask which file to open
+log_file = input("Which file to Analyze? ")
+
+# Get current director
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+# Build File Path
+file_path = os.path.join(script_dir, log_file)
+
+# Open log file
+f = open(file_path, "r")
+
+# Read the file line by line
+while True:
+    line = f.readline()
+    if not line:
+        break
+    # Check for 404
+    if "404" in line:
+        if "administrator" in line:
+            if "%" in line:
+                # Print line
+                print(line)
+            
+            
+        
+# Close File
+f.close()
