2271f6eae9e9d616d1ff9d3982e212226a0bdde7
LarexGame <renealexandergarciatriana@gmail.com>
2025-11-12 09:48:38 -0800

    People in Space and Find Good Passwords

--- start/CH08/HaveIBeenPwned.py
+++ start/CH08/HaveIBeenPwned.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
 # Script that checks passwords agains haveibeenpwned.com API
 # https://haveibeenpwned.com/API/v3#PwnedPasswords
-# By 
+# By Garcia 11/12
+
+import hashlib
+import requests
+
+def SHA1(msg):
+    return hashlib.sha1(msg.encode()).hexdigest()
+def test_password(preffix):
+
+    url = "https://api.pwnedpasswords.com/range/" + preffix
+    payload={}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    # Save response to variable
+    raw_text = response.text
+    # Split on CRLF
+    raw_list = raw_text.split("\r\n")
+    # Separate on :, save to dictionary
+    raw_dict = {}
+    for item in raw_list:
+        split_results = item.split(":")
+        raw_dict[split_results[0]] = split_results[1]
+    # Return dictionary
+    return raw_dict
+
+# Ask User for password
+password = input("Password: ")
+
+# Hash Password
+pass_hash = SHA1(password)
+pass_hash = pass_hash.upper()
+
+# Split password
+hash_preffix = pass_hash[0:5]
+hash_suffix = pass_hash[5:]
+
+# Call API
+results = test_password(hash_preffix)
+
+# Search Results
+if hash_suffix in results.keys():
+    print(f"Password is bad, it has been found {results[hash_suffix]} times")
+else:
+    print("Password is safe")
--- start/CH08/PeopleInSpace1.py
+++ start/CH08/PeopleInSpace1.py
@@ -1,1 +1,39 @@
 # paste code here
+
+import requests
+
+url = "http://api.open-notify.org/astros.json"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+# Convert to Python object
+astros = response.json()
+
+# Print number of people
+print(f"There are currently {astros['number']} people in space")
+
+# Firts person in space
+print("The first person in space is {0} is on craft {1}".format(
+    astros["people"][0]["name"], 
+    astros["people"][0]["craft"] 
+    )
+)
+
+# Second Person in space
+second_person = astros["people"][1]
+print("the second person in space is {0}, on craft {1}".format(
+    second_person["name"],
+    second_person["craft"]
+    )
+)
+
+# Loopthrough all astronauts
+for person in astros["people"]:
+    print("{0} in on {1}".format(
+        person["name"],
+        person["craft"]
+    )
+)
