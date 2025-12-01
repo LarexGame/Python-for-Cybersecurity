73457308da77061e500debc6a622a8586d9e3b8f
LarexGame <renealexandergarciatriana@gmail.com>
2025-12-01 09:10:03 -0800

    tkinter GUI

--- start/CH10/tkinter_button.py
+++ start/CH10/tkinter_button.py
@@ -1,13 +1,39 @@
-# Seconder tkinter script
-# Add a button and command
-# Create 
+# First tkinter script
+# Create by Garcia 12/01
 
 # Import tkinter
+import tkinter
 
-# Functions
+def button_clicked():
+    tkinter.Label(
+        root,
+        text = "Button was Clicked"
+).pack()
+    # update my_label
+    my_label.configure(
+        text = "Python is AWESOME!!",
+        font = ("Courier", 25),
+        fg = "lightyellow",
+        bg = "darkred"
+    )
 
 # Create the GUI main window
+root = tkinter.Tk()
 
 # Add widgets
+my_label = tkinter.Label(
+    root, 
+    text = "Hello World",
+    font = ("Arial Bold", 50)
+    )
+my_label.pack()
+
+my_button = tkinter.Button(
+    root,
+    text = "Click ME",
+    command = button_clicked
+    )
+my_button.pack()
 
 # Enter the main event loop
+root.mainloop()
--- start/CH10/tkinter_hello.py
+++ start/CH10/tkinter_hello.py
@@ -1,10 +1,19 @@
 # First tkinter script
-# Create by 
+# Create by Garcia 12/01
 
 # Import tkinter
+import tkinter
 
 # Create the GUI main window
+root = tkinter.Tk()
 
 # Add widgets
+my_label = tkinter.Label(
+    root, 
+    text = "Hello World",
+    font = ("Arial Bold", 50)
+    )
+my_label.pack()
 
 # Enter the main event loop
+root.mainloop()
